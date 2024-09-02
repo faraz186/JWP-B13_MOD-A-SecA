6376cfb53fe1e3b9c056ca4037f1ac47fcff5d15
Muhammad Faraz <69837700+faraz186@users.noreply.github.com>
2024-09-02 12:18:41 +0500

    Add files via upload

---
+++ JS/Class 01/app.js
@@ -0,0 +1,100 @@
+ alert("faraz") // JS Builtin Function
+
+document.write("faraz")  // JS Builtin method
+
+console.log(456646546)   // JS Builtin method
+
+
+// variables
+
+
+// datatypes
+
+// String, number, undefined
+
+
+var firstname =  "Muhammad";
+
+var lastname = "Faraz"
+
+console.log( firstname + lastname)
+
+
+var abc = 10;
+
+ abc = 15
+
+
+
+var num1 = 10;
+
+num1 = 12;
+
+console.log(num1)
+
+
+
+var fname = "Mr"
+
+var lname = "ahmed"
+
+console.log(fname + " " + lname)
+
+
+var num1 = 10;
+
+var num2 = "20";
+
+console.log(typeof num1)
+
+
+
+// variable declaration
+
+var fname;
+
+console.log(fname)
+
+// variable declaration and initialization
+
+var fname = "Faraz"
+
+console.log(fname)
+
+
+var num = 100 * 10;
+
+var num2 = num + 10
+
+console.log(num2)
+
+
+var firstname = "John";
+
+var lastname = 'Doe';
+
+var age = 18;
+
+var city = "karachi";
+
+console.log("hELLO My name is John Doe. I'm 18 years old. I lived in karachi");
+
+
+console.log("hELLO My name is " + firstname +" " + lastname + ". I'm " + age + " years old" + "<br>" +". I lived in "+city)
+
+
+
+alert("Muhammad \n Faraz")
+
+
+document.write("Muhammad <br> faraz")
+
+console.log("Muhammad \n Faraz");
+
+// camel case
+
+var firstName = 10;
+
+// snake case
+
+var first_name = 10;
